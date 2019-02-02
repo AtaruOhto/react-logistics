5864a19931ab0eae27b59a3cf67e30038620bdf9
Ataru Ohto <ataruohto@gmail.com>
2019-02-02 18:54:45 +0900

    added counter example (ES)

--- examples/counter/src/index.js
+++ examples/counter/src/index.js
@@ -1,8 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { buildStore, ReactLogisticsOption } from 'react-logistics';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+const initialState = {
+  count: 0,
+};
+
+const option = {
+  exposeGlobal: true /* expose store to global object. you can call store.setState() and getState() directly from console. */,
+  saveHistory: true /* saving payload history, enables time machine. */,
+};
+
+const counterStore = buildStore(initialState, 'MyCounterStore', option);
+
+/* Define Actions. It's completely arbitrary to define actions and you can call store.setState() directly */
+export const increment = count => {
+  counterStore.setState({ count: (count += 1) });
+};
+
+export const decrement = count => {
+  counterStore.setState({ count: (count -= 1) });
+};
+
+const CounterComponent = ({ count }) => (
+  <div>
+    <h1>{count}</h1>
+    <button
+      onClick={() => {
+        increment(count);
+      }}
+    >
+      increment
+    </button>
+    <button
+      onClick={() => {
+        decrement(count);
+      }}
+    >
+      decrement
+    </button>
+  </div>
+);
+
+/* Enhance a component with store.withConsumer function. */
+const EnhancedCounter = counterStore.withConsumer(CounterComponent);
+
+const App = () => (
+  <counterStore.Provider>
+    <div>
+      <EnhancedCounter />
+    </div>
+  </counterStore.Provider>
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
